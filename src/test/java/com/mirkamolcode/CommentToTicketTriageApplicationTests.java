b92f5d8e077c0e9bdb0a73a4db580bee8324fcab
Mirkamol <mirkamol.m.a@gmail.com>
2026-05-13 20:10:27 +0500

    feat: Initial project set up

---
+++ src/test/java/com/mirkamolcode/CommentToTicketTriageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirkamolcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentToTicketTriageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
