01ad057aacf1192697a4515774e7a45d8ba29f79
Mirkamol <mirkamol.m.a@gmail.com>
2026-05-16 00:47:09 +0500

    feat: added repositories

---
+++ src/main/java/com/mirkamolcode/repository/TicketRepository.java
@@ -0,0 +1,9 @@
+package com.mirkamolcode.repository;
+
+import com.mirkamolcode.entity.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TicketRepository extends JpaRepository<Ticket, UUID> {
+}
