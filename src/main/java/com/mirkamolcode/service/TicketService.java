0dd7c5ff162acea89aae92614c1a47a9df68ba9f
Mirkamol <mirkamol.m.a@gmail.com>
2026-05-16 09:50:42 +0500

    feat: added service layer

---
+++ src/main/java/com/mirkamolcode/service/TicketService.java
@@ -0,0 +1,43 @@
+package com.mirkamolcode.service;
+
+import com.mirkamolcode.dto.TicketResponse;
+import com.mirkamolcode.entity.Ticket;
+import com.mirkamolcode.repository.TicketRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class TicketService {
+    private final TicketRepository ticketRepository;
+
+    public TicketService(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
+
+    public List<TicketResponse> getAll() {
+        return ticketRepository.findAll()
+                .stream()
+                .map(this::mapToTicketResponse)
+                .toList();
+    }
+    public TicketResponse getById(UUID id) {
+        Ticket ticket = ticketRepository.findById(id)
+                .orElseThrow();
+
+        return mapToTicketResponse(ticket);
+    }
+    private TicketResponse mapToTicketResponse(Ticket ticket) {
+        return new TicketResponse(
+                ticket.getId(),
+                ticket.getTitle(),
+                ticket.getCategory(),
+                ticket.getPriority(),
+                ticket.getSummary(),
+                ticket.getLocalDateTime()
+        );
+    }
+
+
+}
