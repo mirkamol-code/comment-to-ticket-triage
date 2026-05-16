6e84fbd585c2753d15563c1fb45863544de8b6e8
Mirkamol <mirkamol.m.a@gmail.com>
2026-05-16 11:52:14 +0500

    fix: replace IDENTITY strategy with UUID generation for ticket entity

--- src/main/java/com/mirkamolcode/entity/Comment.java
+++ src/main/java/com/mirkamolcode/entity/Comment.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 @Entity
 public class Comment {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String senderEmail;
--- src/main/java/com/mirkamolcode/entity/Ticket.java
+++ src/main/java/com/mirkamolcode/entity/Ticket.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 @Entity
 public class Ticket {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String title;
